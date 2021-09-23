ca5170a7176fb58ce5cd1dd56394f2b6d1f8516f
Evelyn Gomez <evelyn.gomez15@gmail.com>
2021-09-22 23:08:16 -0500

    Completed exercise

---
+++ 01 - JavaScript Drum Kit/drum-kit.js
@@ -0,0 +1,25 @@
+
+function playSound(KeyboardEvent){
+    const audio = document.querySelector(`audio[data-key="${KeyboardEvent.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${KeyboardEvent.keyCode}"]`)
+    console.log(audio);  
+    if(!audio){
+        return;// stop function from running if no key associated. 
+    } 
+    audio.currentTime = 0; //restart from begin; can play with out needing to finish transition 
+    audio.play(); 
+    
+    key.classList.add('playing');     
+}
+
+const keys = document.querySelectorAll('.key'); 
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(KeyboardEvent){
+    if(!KeyboardEvent.propertyName === 'transform'){return; }
+    this.classList.remove('playing'); 
+}
+
+
+
+window.addEventListener('keydown', playSound)  
