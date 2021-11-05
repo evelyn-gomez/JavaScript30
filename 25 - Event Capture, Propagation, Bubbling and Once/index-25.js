82ce48c3be56fbda82c13b6f3aeb411bd8d2633c
Evelyn Gomez <evelyn.gomez15@gmail.com>
2021-11-04 17:51:42 -0700

    examples DOM events and handler

--- 25 - Event Capture, Propagation, Bubbling and Once/index-25.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/index-25.js
@@ -2,24 +2,53 @@ const divs = document.querySelectorAll('div');
 
 function logDivValue(e){
     console.log(this.classList.value); 
-    // e.stopPropagation(); 
+    e.stopPropagation(); 
 }
 
 bod = document.querySelector('.bod'); 
-// bod.addEventListener('click', logDivValue); 
+bod.addEventListener('click', logDivValue, {
+    once: true
+}); 
 
 for(let div of divs){
     div.addEventListener('click', logDivValue, {
-        capture: false, 
         once: true
     })
 }
 
 const btn = document.querySelector('button'); 
-
-btn.addEventListener('click', () => {
-    console.log('click!!!!')
-}, {
-    once: true
+btn.addEventListener('mousedown', (event) => {
+    if (event.button == 0) {
+        console.log("Left button");
+    } else if (event.button == 1) {
+        console.log("Middle button");
+    } else if (event.button == 2) {
+        console.log("Right button");
+    }
 })
+
+const para = document.querySelector('p');
+window.addEventListener("keydown", event => {
+    if (event.key == "v" || event.key == 'V') {
+        para.style.background= "violet";
+    }
+});
+
+window.addEventListener("keyup", event => {
+    if (event.key == "v" || event.key == "V") {
+        para.style.background = "";
+    }
+});
+
+window.addEventListener("keydown", event => {
+    if (event.key == " " && event.ctrlKey) {
+      console.log("Continuing!");
+    }
+  });
+
+
+    
+
+
+
 
